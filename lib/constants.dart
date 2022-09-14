980a2dc345478a1592c34597431c706b393b2f9e
Michael Adeyemo <>
2022-09-14 10:52:09 +0100

    added Address samples

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,16 @@
 import 'models/internet_address.dart';
 
+/// Predefined reliable addresses. This is opinionated but should be enough
 /// The default [internetAddresses] used if non is supplied to [SocketObservingStrategy]
+/// | Address        | Provider   | Info                                            |
+/// |:---------------|:-----------|:------------------------------------------------|
+/// | 1.1.1.1        | CloudFlare | https://1.1.1.1                                 |
+/// | 1.0.0.1        | CloudFlare | https://1.1.1.1                                 |
+/// | 8.8.8.8        | Google     | https://developers.google.com/speed/public-dns/ |
+/// | 8.8.4.4        | Google     | https://developers.google.com/speed/public-dns/ |
+/// | 208.67.222.222 | OpenDNS    | https://use.opendns.com/                        |
+/// | 208.67.220.220 | OpenDNS    | https://use.opendns.com/                        |
+///
 const kDefaultInternetAddresses = [
   InternetAddress(
       host: _googleHost, port: _defaultPort, timeOut: _defaultTimeOut),
